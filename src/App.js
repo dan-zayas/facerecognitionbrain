c5c3bfb14205a1a3884d7f22dcf6fec173d46027
dan-zayas <dmzayasplus@gmail.com>
2023-02-28 08:15:28 -0600

    added some click functionality for event listening

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click")
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+                {/* 
         <FaceRecognition />  */}
+        <ParticlesBg type="fountain" bg={true} />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.gif'
 import './Logo.css';
 
 const Logo = () => {
     return (
             <Tilt
-                style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}
+                style={{ height: '150px', width: '150px', padding: '30px', margin: '20px', backgroundColor: 'aquamarine' }}
                 className='br2 shadow-2 tilt'
                 tiltMaxAngleX={55}
                 tiltMaxAngleY={55}
             >
                 <div style={{}}>
-                    <h1>👀</h1>
+                    <img alt="logo" src={brain} width="80px"/>
                 </div>
             </Tilt>
     );
